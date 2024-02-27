591b0b564a771a9989877aa03a8171d968ddb1e9
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-27 20:06:40 +0100

    feat(MainController): Hero can be moved also with WASD keys

--- wanderer/src/controllers/MainController.java
+++ wanderer/src/controllers/MainController.java
@@ -11,43 +11,47 @@ import java.awt.image.AreaAveragingScaleFilter;
 
 public class MainController implements KeyListener {
 
-    private Board board;
-    private Hero hero;
+  private Board board;
+  private Hero hero;
 
-    private Area area;
+  private Area area;
 
-    public MainController(Board board) {
-        area = new Area();
-        this.hero = new Hero(0, 0);
-        this.board = board;
-        board.setHero(hero);
-        board.setArea(area);
+  public MainController(Board board) {
+    area = new Area();
+    this.hero = new Hero(0, 0);
+    this.board = board;
+    board.setHero(hero);
+    board.setArea(area);
+  }
+
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_UP ||
+        e.getKeyChar() == 'w') {
+      hero.moveUp();
+    } else if (e.getKeyCode() == KeyEvent.VK_DOWN||
+        e.getKeyChar() == 's') {
+      hero.moveDown();
+    } else if (e.getKeyCode() == KeyEvent.VK_LEFT||
+        e.getKeyChar() == 'a') {
+      hero.moveLeft();
+    } else if (e.getKeyCode() == KeyEvent.VK_RIGHT||
+        e.getKeyChar() == 'd') {
+      hero.moveRight();
     }
 
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            hero.moveUp();
-        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            hero.moveDown();
-        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            hero.moveLeft();
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            hero.moveRight();
-        }
-
-        board.repaint();
-    }
+    board.repaint();
+  }
 }
