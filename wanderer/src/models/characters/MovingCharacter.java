67c091c6695d0c73d33afa3322859192df750ba2
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-29 19:19:31 +0100

    chore(MovingCharacters): stats added with vars setters and getters
    added stats:
    - int maxHealth
    - int health
    - int dP
    - int sP
    added setters and getters for all of them

--- wanderer/src/models/characters/MovingCharacter.java
+++ wanderer/src/models/characters/MovingCharacter.java
@@ -1,13 +1,47 @@
 package models.characters;
 
 public class MovingCharacter {
-
+  protected int maxHealth;
+  protected int health;
+  protected int dP;
+  protected int sP;
   protected int x;
   protected int y;
 
   public MovingCharacter(int x, int y) {
     this.x = x;
     this.y = y;
+  }
+  public int getMaxHealth() {
+    return maxHealth;
+  }
+
+  public void setMaxHealth(int maxHealth) {
+    this.maxHealth = maxHealth;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
+  public int getdP() {
+    return dP;
+  }
+
+  public void setdP(int dP) {
+    this.dP = dP;
+  }
+
+  public int getsP() {
+    return sP;
+  }
+
+  public void setsP(int sP) {
+    this.sP = sP;
   }
 
   public int getX() {
