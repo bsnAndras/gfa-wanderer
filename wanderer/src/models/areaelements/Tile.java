f0bf07f5475527326cebaac495d88c553f399540
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-03-02 12:51:57 +0100

    feat(Tile): occupation handling implemented
    new var: boolean isOccupied
    getters and setters set up

--- wanderer/src/models/areaelements/Tile.java
+++ wanderer/src/models/areaelements/Tile.java
@@ -1,4 +1,24 @@
 package models.areaelements;
 
-public class Tile {
+public abstract class Tile {
+  /**
+   * determines whether the tile is occupied by a character
+   */
+  private boolean isOccupied;
+
+  public Tile() {
+    this.isOccupied = false;
+  }
+
+  public boolean isOccupied() {
+    return isOccupied;
+  }
+
+  public void occupy() {
+    isOccupied = true;
+  }
+
+  public void leave() {
+    isOccupied = false;
+  }
 }
