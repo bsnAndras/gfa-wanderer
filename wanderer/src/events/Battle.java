20acdedd15ebb5b705f78eaf9c15930486737356
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-03-02 17:56:43 +0100

    chore(battle): battle method added without real content yet

--- wanderer/src/events/Battle.java
+++ wanderer/src/events/Battle.java
@@ -1,7 +1,22 @@
 package events;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import models.characters.MovingCharacter;
+
 public class Battle {
-  public Battle() {
+  MovingCharacter attacker;
+  MovingCharacter defender;
+
+  public Battle(MovingCharacter attacker, MovingCharacter defender) {
+    this.attacker = attacker;
+    this.defender = defender;
+
+    battle();
+  }
+
+  private boolean battle() {
     System.out.println("Battle happens");
+    return false;
   }
 }
